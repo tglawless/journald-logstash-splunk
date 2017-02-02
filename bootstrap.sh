14c36ebe338f7466b6b1b20fb3b6dfe214bd26f4
tglawless <tglawless@users.noreply.github.com>
2017-02-01 19:49:37 -0500

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-export SPLUNK_HOST=input-prd-p-zfq4x6wf8mv4.cloud.splunk.com
-export SPLUNK_TOKEN=67E90F4D-83B2-414F-9B29-6275C9EA99FC
+# The value of this variable should be in the form of
+# input-<UNIQUE HOSTNAME>.cloud.splunk.com
+export SPLUNK_HOST=<YOUR SPLUNK CLOUD HOSTNAME>
+
+# This value should be the token created by Splunk when
+# a new HTTP Event Collector is created.
+export SPLUNK_TOKEN=<YOUR SPLUNK CLOUD HTTP EVENT COLLECTOR TOKEN>
 
 sudo yum install -y epel-release vim
 sudo yum install -y python-pip gcc python-devel openssl-devel
